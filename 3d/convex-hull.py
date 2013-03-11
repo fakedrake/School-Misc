d3a23d5b6f3eff895db7946b4a4c1f669830c19c
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-03-11 14:09:12 +0200

    First actually workig version

--- 3d/convex-hull.py
+++ 3d/convex-hull.py
@@ -7,8 +7,15 @@ POINT_NUM = 10
 POINTS_FILENAME = "points.png"
 HULL_FILENAME = "convex-hull.png"
 
-MAX_X=100
-MAX_Y=100
+IMAGE_PADDING = (10,10)
+
+POINT_COLOR = (255,0,0)
+
+MAX_X = 1000
+MAX_Y = 500
+
+def vsub(p,q):
+    return tuple(map(lambda x1,x2:x1-x2, p, q))
 
 class Plane(object):
     """The plane with the points.
@@ -22,22 +29,27 @@ class Plane(object):
         else:
             self.points = points
 
-        self.min_p = min(self.points, key=lambda ((x,y)):y)
+        self.min_p = max(self.points, key=lambda ((x,y)):y)
 
     def edges(self):
         for si,start in enumerate(self.points):
             for end in self.points[si+1:]:
                 yield (start, end)
 
-    def ccw(self, (p, q), r):
-        return cmp((q[0] - p[0])*(r[1] - p[1]) - (r[0] - p[0])*(q[1] - p[1]), 0)
+    def ccw(self, (p,q), r):
+        """Return the side of pq that r is found."""
+        x1, y1 = vsub(p, q)
+        x2, y2 = vsub(p, r)
+        return cmp(x1*y2 - x2*y1,0)
 
     def edge_in_hull(self, edge):
         """Test if edge is in hull."""
         side = STRAIGHT
 
         for i in self.points:
+            # If on the correct side or straight or if the side is not determined
             if self.ccw(edge, i) in [side, STRAIGHT] or side == STRAIGHT:
+                # Update side if not locked
                 if side == STRAIGHT:
                     side = self.ccw(edge, i)
             else:
@@ -56,16 +68,18 @@ class Plane(object):
 
     def angle_key(self, p):
         """Return the tangent of the vector to the lowest point."""
-        x,y = tuple(map(lambda x1,x2:x1-x2, p, self.min_p)) # p - min_p
+        x,y = vsub(p, self.min_p) # p - min_p
 
-        if y == 0:
-            return -float('Inf')
+        if x == 0:
+            return 0
 
-        return y/x
+        return float(y)/float(x)
 
     def convex_hull(self):
+        """This is the stable way to get the convex hull."""
         hull = self.dumb_convex_hull()
-        return sorted(hull, key=self.angle_key)
+        print "Sorting %d points" % len(hull)
+        return sorted(hull, key=self.angle_key, reverse=True)
 
 
 class Visualize(object):
@@ -80,31 +94,40 @@ class Visualize(object):
     def add_point(self, coord, image):
         draw.point(coord)
 
-    def points_image(self, image_filename=POINTS_FILENAME, image=None):
+    def points_image(self, image_filename=POINTS_FILENAME, image=None, color=POINT_COLOR):
         """Draw points of plane on image."""
 
         if image is None:
-            image = Image.open(image_filename, 0x777)
+            image = Image.new("RGB", self.geometry())
 
         draw = ImageDraw.Draw(image)
-        draw.point(self.plane.points)
+        draw.point(self.plane.points, fill=color)
 
         return image
 
-    def geometry(self):
-        return max(self.plane.points, key=lambda (x,y): x)[0], max(self.plane.points, key=lambda (x,y): y)[1]
+    def geometry(self, padding=IMAGE_PADDING):
+        min_geo = max(self.plane.points, key=lambda (x,y): x)[0], max(self.plane.points, key=lambda (x,y): y)[1]
 
-    def convex_hull_image(self, hull_filename=HULL_FILENAME, hull_image=None):
-        if hull_image is None:
-            hull_image = Image.new("RGB", self.geometry())
+        return tuple(map(lambda x,y:x+y,min_geo, padding))
 
-        draw = ImageDraw.Draw(hull_image)
-        draw.polygon(self.plane.convex_hull())
+    def convex_hull_image(self, hull_filename=HULL_FILENAME, image=None):
+        if image is None:
+            image = Image.new("RGB", self.geometry())
 
-        return hull_image
+        draw = ImageDraw.Draw(image)
+
+        chull = self.plane.convex_hull()
+
+        draw.polygon(chull)
+
+        return image
 
 
 if __name__ == "__main__":
-    p = Plane(10)
+    from sys import argv
+
+    p = Plane( int(argv[1]) )
     s = Visualize(p)
-    s.convex_hull_image().show()
+    points = s.points_image()
+    hull = s.convex_hull_image(image=points)
+    points.show()
